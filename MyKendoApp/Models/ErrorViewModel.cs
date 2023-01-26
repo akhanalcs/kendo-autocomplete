a20786912884bafe686a67b123117337c71b73a1
Ashish Khanal <AKhanal@marathonpetroleum.com>
2023-01-26 13:55:50 -0500

    Initial Commit

---
+++ MyKendoApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyKendoApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
